18b4da43bbd3687f5568c96a26b0d6eaecb17fc2
billyAdi <billz.wew@gmail.com>
2018-03-03 21:20:03 +0700

    Test

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.user.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, View.OnTouchListener, GestureDetector.OnGestureListener{
+    protected Button button1,button2;
+    protected Spinner spinner;
+    protected ImageView iv;
+    protected GestureDetector mGestureDetector;
+    protected Bitmap mBitmap;
+    protected Canvas mCanvas;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.button1=this.findViewById(R.id.btn_add1);
+        this.button2=this.findViewById(R.id.btn_add2);
+        this.spinner=this.findViewById(R.id.spinner);
+        this.iv=this.findViewById(R.id.iv);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.operator, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        this.mGestureDetector=new GestureDetector(this,this);
+        this.spinner.setAdapter(adapter);
+        this.spinner.setOnItemSelectedListener(this);
+        this.button1.setOnClickListener(this);
+        this.button2.setOnClickListener(this);
+
+
+    }
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        this.mBitmap=Bitmap.createBitmap(this.iv.getWidth(),this.iv.getHeight(),Bitmap.Config.ARGB_8888);
+        this.iv.setImageBitmap(mBitmap);
+        this.mCanvas=new Canvas(mBitmap);
+
+        int mColorBackground= ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null);
+        mCanvas.drawColor(mColorBackground);
+        this.iv.invalidate();
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return true;
+    }
+}
