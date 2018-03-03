9c49de4db8fd6e3603857400ec1c976e854ee9aa
Ferdinandus Renaldi <ferdinandus.renaldi@gmail.com>
2018-03-03 22:04:37 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/user/myapplication/ThreadActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+/**
+ * Created by WIN 10 on 3/3/2018.
+ */
+
+public class ThreadActivity implements  Runnable{
+    private MainActivity main;
+    public ThreadActivity(MainActivity main){
+        this.main=main;
+    }
+    @Override
+    public void run() {
+        main.mBitmap= Bitmap.createBitmap(main.iv.getWidth(),main.iv.getHeight(),Bitmap.Config.ARGB_8888);
+        main.iv.setImageBitmap(main.mBitmap);
+        main.mCanvas=new Canvas(main.mBitmap);
+        main.drawKotak();
+        //int mColorBackground= ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null);
+        //mCanvas.drawColor(mColorBackground);
+        main.iv.invalidate();
+    }
+}
