680dff3c26714ab3580febb59b5bcbce792a0939
Ferdinandus Renaldi <ferdinandus.renaldi@gmail.com>
2018-03-04 16:15:43 +0700

    Update Hitungan.java

--- app/src/main/java/com/example/user/myapplication/Hitungan.java
+++ app/src/main/java/com/example/user/myapplication/Hitungan.java
@@ -1,10 +1,12 @@
+boolean nextAngka=true;
+
 public boolean parentheses(String bacaan){
     Stack simbol = new Stack();
     for(int i = 0 ; i<bacaan.length();i++){
-      if(bacaan.charAt(i)==(){
+      if(bacaan.charAt(i)=='('){
         simbol.push(bacaan.charAt(i);
         }
-        else if(bacaan.charAt(i)==)){
+        else if(bacaan.charAt(i)==')'){
           if(simbol.isEmpty()){
             return false;
           }
@@ -22,3 +24,23 @@ public boolean parentheses(String bacaan){
     
     
 }
+         
+        public boolean valid(char next){
+          if(next=='+'|| next=='-' || next == '/' || next == '*'){
+                if(nextAngka==true){
+                    return false;   
+                }
+              else{
+                nextAngka=true;
+                  return true;
+              }
+          }
+            else{
+                if(nextAngka==true){
+                    nextAngka=false;
+                    return true;
+                }
+                }
+        }
+         
+         
