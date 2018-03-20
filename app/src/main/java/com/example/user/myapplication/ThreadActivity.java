134972c36a638ec1417d7b91bdcf59317489a67b
Ferdinandus Renaldi <ferdinandus.renaldi@gmail.com>
2018-03-20 19:26:02 +0700

    Update ThreadActivity.java

--- app/src/main/java/com/example/user/myapplication/ThreadActivity.java
+++ app/src/main/java/com/example/user/myapplication/ThreadActivity.java
@@ -2,6 +2,9 @@ package com.example.user.myapplication;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 /**
  * Created by WIN 10 on 3/3/2018.
@@ -9,21 +12,31 @@ import android.graphics.Canvas;
 
 public class ThreadActivity implements  Runnable{
     private MainActivity main;
-    private boolean sudahPernahDibuat;
-    public ThreadActivity(MainActivity main,boolean status){
-        this.sudahPernahDibuat=status;
+
+
+    public ThreadActivity(MainActivity main){
+
         this.main=main;
+
     }
+
     @Override
     public void run() {
-        this.main.mBitmap= Bitmap.createBitmap(main.iv.getWidth(),main.iv.getHeight(),Bitmap.Config.ARGB_8888);
-        this.main.iv.setImageBitmap(main.mBitmap);
-        this.main.mCanvas=new Canvas(main.mBitmap);
-        this.main.initializeCanvas();
-        this.main.iv.invalidate();
-        if(sudahPernahDibuat){
-            this.main.updatePosisiKotak();
-        }
+
+            this.main.mBitmap= Bitmap.createBitmap(main.iv.getWidth(),main.iv.getHeight(),Bitmap.Config.ARGB_8888);
+            this.main.iv.setImageBitmap(main.mBitmap);
+            this.main.mCanvas=new Canvas(main.mBitmap);
+            this.main.initializeCanvas();
+            this.main.iv.invalidate();
+
+            if(main.presenter.tempIsi!=null) {
+                main.presenter.isiKotak = main.presenter.tempIsi;
+                this.main.updatePosisiKotak();
+            }
+
+
+
+
 
     }
 }
